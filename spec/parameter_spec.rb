e6c5221cf747f5c10c2fb725dd4b308a4989e31f
Metallion <akieckens@axsh.net>
2014-04-02 19:39:21 +0900

    Improved unit test code a bit

--- spec/parameter_spec.rb
+++ spec/parameter_spec.rb
@@ -3,12 +3,12 @@
 require "spec_helper"
 
 describe "parameter" do
+  let(:body) { JSON.parse(last_response.body) }
+
   it "throws away parameters that weren't defined" do
-    get("features/remove_undefined", a: "a", b: "b", c: "c") do |response|
-      body = JSON.parse(response.body)
-      expect(body.member?('a')).to eq true
-      expect(body.member?('b')).to eq true
-      expect(body.member?('c')).to eq false
-    end
+    get("features/remove_undefined", a: "a", b: "b", c: "c")
+    expect(body.member?('a')).to eq true
+    expect(body.member?('b')).to eq true
+    expect(body.member?('c')).to eq false
   end
 end
