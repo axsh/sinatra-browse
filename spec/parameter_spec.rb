0f8b86522a129bb9dc4372103eea57523284ba65
Metallion <akieckens@axsh.net>
2014-04-03 11:39:03 +0900

    Wrote test for string validation 'in'

--- spec/parameter_spec.rb
+++ spec/parameter_spec.rb
@@ -3,7 +3,8 @@
 require "spec_helper"
 
 describe "sinatra-browse" do
-  let(:body) { JSON.parse(last_response.body) }
+  def body; JSON.parse(last_response.body) end
+  def status; last_response.status end
 
   it "throws away parameters that weren't defined" do
     get("features/remove_undefined", a: "a", b: "b", c: "c")
@@ -12,8 +13,6 @@ describe "sinatra-browse" do
     expect(body.member?('c')).to eq false
   end
 
-  # This is just a regression test for now
-  #TODO: This seems to happen when defining a route without a slash. Do something about that
   it "doesn't crash when calling a route that wasn't defined" do
     get("i_dont_exist")
     expect(last_response.errors).to be_empty
@@ -59,5 +58,23 @@ describe "sinatra-browse" do
   end
 
   describe "String validation" do
+    describe "in" do
+      context "with a value present the array provided" do
+        it "allows the parameter to go through" do
+          get("features/string_validation", in: "joske")
+          expect(body['in']).to eq('joske')
+          get("features/string_validation", in: "jefke")
+          expect(body['in']).to eq('jefke')
+        end
+      end
+
+      context "with a value not present in the array provided" do
+        it "fails with a 400 status" do
+          get("features/string_validation", in: "jantje")
+          expect(status).to eq 400
+          #TODO: Check for error message?
+        end
+      end
+    end
   end
 end
