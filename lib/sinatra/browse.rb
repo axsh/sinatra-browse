5d8afc4c22e03771ffcccc18ae15874dbf8cdd71
Metallion <akieckens@axsh.net>
2014-04-03 15:44:41 +0900

    Made a quick and dirty markdown format for documentation

--- lib/sinatra/browse.rb
+++ lib/sinatra/browse.rb
@@ -60,7 +60,7 @@ module Sinatra::Browse
     end
 
     # Create the (future) browsable api
-    app.param :format, :String, in: ["kusohtml"], default: "kusohtml"
+    app.param :format, :String, in: ["kusohtml", "markdown"], default: "markdown"
     app.get '/browse' do
       Sinatra::Browse.format(params["format"], app.browse_routes).generate
     end
--- lib/sinatra/browse/format.rb
+++ lib/sinatra/browse/format.rb
@@ -2,14 +2,21 @@
 
 module Sinatra::Browse
   def self.format(f, browse_routes)
-    KusoHtml.new(browse_routes) if f == "kusohtml"
+    case f
+    when "kusohtml"
+      KusoHtml.new(browse_routes)
+    when "markdown"
+      Markdown.new(browse_routes)
+    end
   end
 
-  class KusoHtml
+  class BrowseFormat
     def initialize(browse_routes)
       @browse_routes = browse_routes
     end
+  end
 
+  class KusoHtml < BrowseFormat
     def generate
       output = ""
       @browse_routes.each { |name, route|
@@ -21,6 +28,21 @@ module Sinatra::Browse
         output += "</ul>"
       }
       output
+    end
+  end
+
+  class Markdown < BrowseFormat
+    def generate
+      output = ""
+      @browse_routes.each { |name, route|
+        output += "# #{name}\n\n"
+        output += "#{route.description}\n\n"
+        route.parameters.each { |param_key, param_value|
+          output += "* #{param_key} #{param_value.to_s}\n"
+        }
+        output += "\n"
+      }
+      output.gsub("\n", "<br />")
     end
   end
 end
