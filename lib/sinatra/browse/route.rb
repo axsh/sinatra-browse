ebf14b7133e1a17bbc335a1ff3c0261f66b0e037
Metallion <akieckens@axsh.net>
2014-07-10 17:01:08 +0900

    Moved the validator class to its own file

--- lib/sinatra/browse/route.rb
+++ lib/sinatra/browse/route.rb
@@ -1,25 +1,6 @@
 # -*- coding: utf-8 -*-
 
 module Sinatra::Browse
-  class Validator
-    attr_reader :criteria
-    attr_reader :value
-    attr_reader :name
-
-    def initialize(map)
-      @name = map[:name]
-      @criteria = map[:criteria]
-      @validation_blk = map[:validation_blk]
-    end
-
-    def validate(param_name, params)
-      @value = params[param_name]
-      @params = params
-
-      instance_exec @criteria, &@validation_blk
-    end
-  end
-
   class Route
     attr_reader :parameters
     attr_reader :name
