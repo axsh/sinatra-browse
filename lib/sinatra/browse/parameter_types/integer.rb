0d52732c5b2f71bd14077e057e70110e4fb52b98
Metallion <akieckens@axsh.net>
2014-09-05 18:21:37 +0900

    Added min and max validators to the integer type

--- lib/sinatra/browse/parameter_types/integer.rb
+++ lib/sinatra/browse/parameter_types/integer.rb
@@ -3,5 +3,8 @@
 module Sinatra::Browse
   parameter_type(:Integer) do
     coerce { |value| Integer(value) }
+
+    validator(:min) { |min| @value >= min }
+    validator(:max) { |max| @value <= max }
   end
 end
