6e79945c4225a88c07cc97af7641b4ded9de5806
Metallion <akieckens@axsh.net>
2014-09-05 19:19:10 +0900

    Moved error handling spec into their own file

--- spec/sinatra-browse_spec.rb
+++ spec/sinatra-browse_spec.rb
@@ -184,33 +184,6 @@ describe "sinatra-browse" do
     end
   end
 
-  describe "on_error" do
-    it "allows for custom error handling" do
-      get("features/error_handing", error: 20)
-      expect(status).to eq 200
-      expect(last_response.body).to eq "we can handle it"
-    end
-  end
 
-  describe "self.default_on_error" do
-    def app; StandardErrorOverrideApp end
-
-    it "allows the user to define default behaviour on parameter errors" do
-      get "/features/default_error_override", a: "b", b: "bbb"
-      expect(status).to eq 400
-      expect(last_response.body).to eq "we had an error"
-    end
-
-    it "allows the user to fall back to standard behaviour by calling _default_on_error" do
-      get "/features/default_error_override", a: "a", b: "we need more b"
-      expect(status).to eq 400
-      expect(body).to eq({
-        "error"=>"parameter validation failed",
-        "parameter"=>"b",
-        "value"=>"we need more b",
-        "reason"=>"format"
-      })
-    end
-  end
 
 end
