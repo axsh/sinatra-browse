ca98567ece717349130150a8b6b3b5f8230b6b55
Metallion <akieckens@axsh.net>
2014-09-05 19:28:49 +0900

    moved coercion related tests into their own file

--- spec/sinatra-browse_spec.rb
+++ spec/sinatra-browse_spec.rb
@@ -17,34 +17,4 @@ describe "sinatra-browse" do
     expect(last_response.errors).to be_empty
   end
 
-  it "coerces parameters into the defined types" do
-    get("features/type_coercion",
-      string: "joske",
-      integer: "1",
-      boolean: "false",
-      float: "1.5",
-    )
-
-    expect(body['string']).to be_a(String)
-    expect(body['integer']).to be_a(Integer)
-    expect(body['boolean']).to be_a(FalseClass)
-    expect(body['float']).to be_a(Float)
-  end
-
-  describe "Boolean coercion" do
-    ["y", "yes", "t", "true", "1"].each do |i|
-      it "returns true for '#{i}'" do
-        get("features/type_coercion", boolean: i)
-        expect(body['boolean']).to be_a(TrueClass)
-      end
-    end
-
-    ["n", "no", "f", "false", "0"].each do |i|
-      it "returns false for '#{i}'" do
-        get("features/type_coercion", boolean: i)
-        expect(body['boolean']).to be_a(FalseClass)
-      end
-    end
-  end
-
 end
---
+++ spec/type_coercion_spec.rb
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+require "spec_helper"
+
+describe "type coercion" do
+  it "coerces parameters into the defined types" do
+    get("features/type_coercion",
+      string: "joske",
+      integer: "1",
+      boolean: "false",
+      float: "1.5",
+    )
+
+    expect(body['string']).to be_a(String)
+    expect(body['integer']).to be_a(Integer)
+    expect(body['boolean']).to be_a(FalseClass)
+    expect(body['float']).to be_a(Float)
+  end
+
+  describe "Boolean coercion" do
+    ["y", "yes", "t", "true", "1"].each do |i|
+      it "coerces true for '#{i}'" do
+        get("features/type_coercion", boolean: i)
+        expect(body['boolean']).to be_a(TrueClass)
+      end
+    end
+
+    ["n", "no", "f", "false", "0"].each do |i|
+      it "coerces false for '#{i}'" do
+        get("features/type_coercion", boolean: i)
+        expect(body['boolean']).to be_a(FalseClass)
+      end
+    end
+  end
+end
