9a1f52673f7dfe654432dc5d0a18fc0d1e4d957a
Metallion <akieckens@axsh.net>
2014-09-05 19:24:02 +0900

    Separated tests for validation that's common to all parameter types

--- spec/sinatra-browse_spec.rb
+++ spec/sinatra-browse_spec.rb
@@ -67,59 +67,9 @@ describe "sinatra-browse" do
     end
   end
 
-  describe "default values" do
-    context "with simple values" do
-      it "sets default values for parameters that weren't provided" do
-        get("features/default")
-        expect(body['a']).to eq('yay')
-        expect(body['b']).to eq(11)
-        expect(body['c']).to eq(false)
-      end
-    end
-
-    context "with a proc" do
-      it "will call the proc and set the result as the default value" do
-        get("features/default_proc")
-        expect(body['a']).to eq(2)
-      end
-    end
-  end
-
-  describe "transform" do
-    it "does a to_proc on whatever was given and calls it on the parameter" do
-      get("features/string_validation", transform: "joske")
-      expect(body["transform"]).to eq("JOSKE")
-    end
-
-    #TODO: Define behaviour for something that doesn't respond to to_proc
-  end
 
 
 
-
-  describe "depends_on" do
-    it "accepts parameter 'a' only when parameter 'b' is also present" do
-      get("features/depends_on", a: "lol")
-      expect(status).to eq 400
-      get("features/depends_on", a: "lol", b: "lul")
-      expect(body["a"]).to eq("lol")
-      expect(body["b"]).to eq("lul")
-    end
-
-    it "accepts parameter 'b' even when 'a' is not present" do
-      get("features/depends_on", b: "lul")
-      expect(body["b"]).to eq("lul")
-    end
-  end
-
-  describe "required" do
-    it "fails when a required parameter wasn't supplied" do
-      get("features/required", a: "a cow")
-      expect(body["a"]).to eq("a cow")
-      get("features/required")
-      expect(status).to eq 400
-    end
-  end
 
 
 
