0e00a0e2bf4e418c5745a3e4760f2a6d9a00245e
Metallion <akieckens@axsh.net>
2015-07-07 17:19:59 +0900

    Updated the version_bump.sh script to include a changelog update

--- version_bump.sh
+++ version_bump.sh
@@ -5,6 +5,7 @@ set -e
 new_version=$1
 today=$(date +"%Y-%m-%d")
 gemspec=sinatra-browse.gemspec
+changelog=CHANGELOG.md
 
 if [ -z "${new_version}" ]; then
   echo "Usage: $0 <version_number>"
@@ -13,13 +14,14 @@ fi
 
 sed -i "s/  s.version     = .*/  s.version     = '${new_version}'/g" ${gemspec}
 sed -i "s/  s.date        = .*/  s.date        = '${today}'/g" ${gemspec}
+sed -i "s/## \[Unreleased\]/## [${new_version}] - ${today}/g" ${changelog}
 
-git diff sinatra-browse.gemspec
+git diff ${gemspec} ${changelog}
 
 read -p "Is this ok? (y/n) " -n 1 -r
 echo
 if [[ ! $REPLY =~ ^[Yy]$ ]]; then
-  git checkout ${gemspec}
+  git checkout ${gemspec} ${changelog}
   exit 1
 fi
 
